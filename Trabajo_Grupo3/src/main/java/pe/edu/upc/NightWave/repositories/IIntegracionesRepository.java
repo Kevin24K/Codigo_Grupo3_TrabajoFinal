77a75bf27a3810a17be52ee624f93ef0afbf1469
EduardAC159 <eduard_somos@hotmail.com>
2025-09-16 08:47:29 -0500

    Add files via upload

---
+++ Trabajo_Grupo3/src/main/java/pe/edu/upc/NightWave/repositories/IIntegracionesRepository.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.NightWave.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.NightWave.entities.Integraciones;
+
+public interface IIntegracionesRepository extends JpaRepository<Integraciones, Integer> {
+}
