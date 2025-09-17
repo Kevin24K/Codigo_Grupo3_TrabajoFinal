981e4bfbfc6b6e39caccf56937a3e1cad842b129
EduardAC159 <eduard_somos@hotmail.com>
2025-09-17 17:44:33 -0500

    Delete Trabajo_Grupo3/src directory

--- Trabajo_Grupo3/src/main/java/pe/edu/upc/NightWave/repositories/IIntegracionesRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.NightWave.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.NightWave.entities.Integraciones;
-
-public interface IIntegracionesRepository extends JpaRepository<Integraciones, Integer> {
-}
--- Trabajo_Grupo3/src/main/java/pe/edu/upc/NightWave/repositories/IMeditacionRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.NightWave.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.NightWave.entities.Meditaciones;
-
-@Repository
-public interface IMeditacionRepository extends JpaRepository<Meditaciones, Integer> { }
