70f2ff1f0baef45e59c37a537abd81bc96158572
EduardAC159 <eduard_somos@hotmail.com>
2025-09-17 18:08:44 -0500

    Add files via upload

---
+++ Trabajo_Grupo3/src/main/java/pe/edu/upc/NightWave/dtos/RecompensasDTO.java
@@ -0,0 +1,50 @@
+package pe.edu.upc.NightWave.dtos;
+
+public class RecompensasDTO
+{
+    private int idRecompensa;
+    private String nombreRecompensa;
+    private String descripcion;
+    private String tipoRecompensa;
+    private int puntosValor;
+
+    public int getIdRecompensa() {
+        return idRecompensa;
+    }
+
+    public void setIdRecompensa(int idRecompensa) {
+        this.idRecompensa = idRecompensa;
+    }
+
+    public String getNombreRecompensa() {
+        return nombreRecompensa;
+    }
+
+    public void setNombreRecompensa(String nombreRecompensa) {
+        this.nombreRecompensa = nombreRecompensa;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipoRecompensa() {
+        return tipoRecompensa;
+    }
+
+    public void setTipoRecompensa(String tipoRecompensa) {
+        this.tipoRecompensa = tipoRecompensa;
+    }
+
+    public int getPuntosValor() {
+        return puntosValor;
+    }
+
+    public void setPuntosValor(int puntosValor) {
+        this.puntosValor = puntosValor;
+    }
+}
