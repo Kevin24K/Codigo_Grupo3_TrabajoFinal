84383004e72f3ea68345c50974a14478719376d9
Kevin24K <kevinhuancahuaman@gmail.com>
2025-11-24 17:01:21 -0500

    Delete Frontend_Grupo3/Frontend_Grupo3 directory

--- Frontend_Grupo3/Frontend_Grupo3/src/app/models/MusicaMultimedia.ts
+++
@@ -1,8 +0,0 @@
-import { TipoMusica } from "./TipoMusica";
-
-export class Musicamultimedia {
-    idMusicaMultimedia: number = 0;
-    nombreMusica: string = "";
-    linkArchivo: string = "";
-    idTipoMusica: TipoMusica = new TipoMusica();
-}
